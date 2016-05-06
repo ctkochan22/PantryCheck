5c79311f3d76043c36cf3ea46c19b1522e1f7189
ctkochan22 <ctkochan22@gmail.com>
2016-05-06 16:52:43 -0700

    Add user routes, make user#index the root

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20160504042609) do
 
   create_table "pantries", force: :cascade do |t|
     t.string   "name"
+    t.boolean  "is_private"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -35,7 +36,7 @@ ActiveRecord::Schema.define(version: 20160504042609) do
   create_table "users", force: :cascade do |t|
     t.integer  "pantry_id"
     t.integer  "personalstore_id"
-    t.string   "phone_number"
+    t.string   "phone_number",     null: false
     t.string   "first_name"
     t.string   "last_name"
     t.datetime "created_at",       null: false
