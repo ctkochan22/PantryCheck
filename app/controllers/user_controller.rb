67b55c84ac4796d96577e7b81baa21bf7cf7c93a
ctkochan22 <ctkochan22@gmail.com>
2016-05-07 17:55:39 -0700

    Add sign-up vertical slice
    Breakdown by file:
    new.html.erb
    - Add new file for sign up form
    - Add form under action 'create'
    
    user_controller
    - Add #new, #show, #create
    - Add user_params private method

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,35 @@
 class UserController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
+    else
+      redirect_to new_user_path
+    end
+  end
+
+  def show
+    puts "********"
+    puts params
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :phone_number,
+      :password
+      )
+  end
 end
+
