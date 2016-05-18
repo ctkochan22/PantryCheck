533416c220408abcb2e9f2fbbbdf82abd3a33a91
ctkochan22 <ctkochan22@gmail.com>
2016-05-18 15:09:49 -0700

    Add second form

--- app/controllers/ingredient_controller.rb
+++ app/controllers/ingredient_controller.rb
@@ -1,15 +1,22 @@
 class IngredientController < ApplicationController
 
   def new
+    @categories = Ingredient.categories_array
+    @storage_types = Ingredient.storage_types_array
   end
 
   def create
-    @user = User.find_by(id: current_user)
-
+    redirect_to action: 'finish', name: ingredient_params[:name], category: ingredient_params[:category], storage_type: ingredient_params[:storage_type]
   end
 
+  def finish
+    @package = params
+  end
 
-
+  def complete
+    puts "******"
+    puts amount_params
+  end
 
   private
 
@@ -23,6 +30,9 @@ class IngredientController < ApplicationController
 
   def amount_params
     params.require(:ingredient).permit(
+      :name,
+      :category,
+      :storage_type,
       :amount
       )
   end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,43 @@
 class Ingredient < ActiveRecord::Base
   belongs_to :pantry
+
+
+  @@categories = {
+    0 => 'Please Select--',
+    1 => 'Beverage',
+    2 => 'Bread/Bakery',
+    3 => 'Canned/Jarred Goods',
+    4 => 'Dairy',
+    5 => 'Dry/Baking Goods',
+    6 => 'Frozen Goods',
+    7 => 'Meat',
+    8 => 'Spice',
+    9 => 'Produce',
+    10 => 'Misc'
+  }
+
+  @@storage_types = {
+    0 => 'Please Select--',
+    1 => 'Basic (Full, half, almost empty, etc.)',
+    2 => 'Number (0.5, 1, 2, etc.)'
+  }
+
+
+
+
+  def self.categories_array
+    categories_array = []
+    @@categories.each do |key, value|
+      categories_array << [value, key]
+    end
+    return categories_array
+  end
+
+  def self.storage_types_array
+    storage_array = []
+    @@storage_types.each do |key, value|
+      storage_array << [value, key]
+    end
+    return storage_array
+  end
 end
