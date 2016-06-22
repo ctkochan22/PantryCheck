888fb1d44ee191b2b408cc34bcb613cdfc29650f
ctkochan22 <ctkochan22@gmail.com>
2016-06-22 16:32:59 -0700

    Add touches to seach route to prepare for ajax

--- app/controllers/ingredient_controller.rb
+++ app/controllers/ingredient_controller.rb
@@ -56,9 +56,14 @@ class IngredientController < ApplicationController
 ##
 
   def search
+    regex = "(.|)#{params[:search_term]}(.|)"
+    term = Regexp.new(regex)
     @ingredients = Ingredient.where(pantry_id: current_pantry.id)
+    @ingredients = @ingredients.select { |item| item[:search_name] =~ term }
+    puts "*******"
+    puts @ingredients
     if request.xhr?
-      return
+      return @ingredients
     else
       redirect_to user_path(current_user.id)
     end
