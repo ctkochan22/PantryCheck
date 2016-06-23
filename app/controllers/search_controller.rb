791ea2110d3ad889b1c4bcbaf2efd30125fb896c
ctkochan22 <ctkochan22@gmail.com>
2016-06-23 16:07:34 -0700

    Add search form to user#show
    --File Breakdown--
    User#show.html
    - Add bootstrap form tag for search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,17 @@
 class SearchController < ApplicationController
+
   def show
+    regex = "(.|)#{params[:search_term]}(.|)"
+    term = Regexp.new(regex)
+    @ingredients = Ingredient.where(pantry_id: current_pantry.id)
+    @ingredients = @ingredients.select { |item| item[:search_name] =~ term }
+    puts "*******"
+    puts @ingredients
+    if request.xhr?
+      return @ingredients
+    else
+      redirect_to user_path(current_user.id)
+    end
   end
+
 end
