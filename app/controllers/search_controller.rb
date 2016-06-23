75698173592b8d15f11214c8294856edb7975f05
ctkochan22 <ctkochan22@gmail.com>
2016-06-23 16:21:06 -0700

    Add search form, function, and results
    --File Breakdown--
    search_controller.rb
    - Add show route
    - Displays results on show page
    
    User#show.html
    - Add form as an inline search bar
    
    Search#show.html
    - Add results that display search term
    - Add results of that search term

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,16 +1,17 @@
 class SearchController < ApplicationController
 
   def show
-    regex = "(.|)#{params[:search_term]}(.|)"
+    current_user #Must for current_pantry
+
+    @search_term = params[:search_term]
+    regex = "(.|)#{@search_term.downcase}(.|)"
     term = Regexp.new(regex)
     @ingredients = Ingredient.where(pantry_id: current_pantry.id)
     @ingredients = @ingredients.select { |item| item[:search_name] =~ term }
-    puts "*******"
-    puts @ingredients
     if request.xhr?
       return @ingredients
     else
-      redirect_to user_path(current_user.id)
+      @ingredients
     end
   end
 
